bae5df922842ee71bca2cf24139017675a39f1aa
David Harrigan <davidharrigan@users.noreply.github.com>
2019-01-12 23:22:01 -0500

    update invite code

--- js/scripts.js
+++ js/scripts.js
@@ -212,13 +212,16 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== '63a63d710875a3a5b96a754997ba3001') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbz8bffMSUDvNx7eW-rMcNhiaCi6VFYgtRKCICMb/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbxbNUD0awhpo4dQ4_vIaSA6ZvEzdZ2YYDF7pMGaMA/exec', data)
                 .done(function (data) {
                     console.log(data);
+                    if (data.hasOwnProperty('error')) {
+                        $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+                        return
+                    }
                     $('#alert-wrapper').html('');
                     $('#rsvp-modal').modal('show');
                 })
